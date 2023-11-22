34d3c873b957815fad28682892b930a22795b6f2
j-o-phillips <jphillips105@googlemail.com>
2023-11-22 12:49:59 +0100

    allow all cors

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,8 @@ ALLOWED_HOSTS = [
     'spaceacademy-backend.railway.internal'
 ]
 
-CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ['http://localhost:5173']
 
 # Application definition
 
